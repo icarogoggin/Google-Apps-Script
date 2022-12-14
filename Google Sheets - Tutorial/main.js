b3869bcb73ee8e2afb2b02e9864b43734024c5fb
Ícaro Goggin <icaro.goggin@gmail.com>
2022-12-14 10:20:53 -0300

    Initial commit

---
+++ Google Sheets - Tutorial/main.js
@@ -0,0 +1,17 @@
+
+function spreadsheet_get(){
+    // a planilha que vamos abrir
+    // let ss = SpreadsheetApp.getActiveSpreadsheet(); // pegar planilha ativa
+    // let ss = SpreadsheetApp.openByName('Google Sheets - Tutorial'); // pegar planilha pelo nome
+    // let ss = SpreadsheetApp.openByUrl('https://docs.google.com/spreadsheets/d/1vESlHNqLk0SfA9pIRB52Y7PuywsIMT1Mw496qQd0tOU/edit#gid=0'); // pegar planilha pelo URL
+    let ss = SpreadsheetApp.openById('1vESlHNqLk0SfA9pIRB52Y7PuywsIMT1Mw496qQd0tOU'); // pegar planilha pelo ID
+    // a folha que vamos interagir
+    // let sheet = ss.getSheetByName('Planilha1'); // pegar folha pelo nome
+    let sheet = ss.getSheets()[0]; // pegar primeira folha
+    // pegar dados da folha
+    let data = sheet.getDataRange().getValues();
+    // pegar dados da linha 1
+    let data1 = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues();
+    Logger.log(data1);
+}
+
