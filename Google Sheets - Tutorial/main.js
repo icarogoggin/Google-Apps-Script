b18bb4ab9ce8caf0a879e9d79fe3d4401f3a03e2
Ícaro Goggin <icaro.goggin@gmail.com>
2022-12-15 09:38:36 -0300

    Update main.js

--- Google Sheets - Tutorial/main.js
+++ Google Sheets - Tutorial/main.js
@@ -21,14 +21,99 @@ function spreadsheet_get(){
 // usando como referencia os conceitos encontrados no link: https://developers.google.com/apps-script/reference/spreadsheet
 
 // get - pegar dados
-function spreadsheet_read(){}
+function spreadsheet_read(spreadsheetId) {
+    try {
+      // Verifica se o ID da planilha é válido
+      if (!spreadsheetId || typeof spreadsheetId !== 'string') {
+        throw new Error('O ID da planilha é inválido.');
+      }
+  
+      // Obtém a planilha com o ID especificado
+      var sheet = SpreadsheetApp.openById(spreadsheetId).getSheets()[0];
+  
+      // Obtém os dados da planilha
+      var data = sheet.getDataRange().getValues();
+  
+      return data;
+    } catch (error) {
+      // Em caso de erro, imprime a mensagem de erro no console e retorna null
+      console.error(error);
+      return null;
+    }
+  }
 
 // post - criar dados
-function spreadsheet_create(){}
+function spreadsheet_create(spreadsheetId, data) {
+    try {
+      // Verifica se o ID da planilha é válido
+      if (!spreadsheetId || typeof spreadsheetId !== 'string') {
+        throw new Error('O ID da planilha é inválido.');
+      }
+  
+      // Verifica se os dados fornecidos são um array válido
+      if (!data || !Array.isArray(data)) {
+        throw new Error('Os dados fornecidos são inválidos.');
+      }
+  
+      // Obtém a planilha com o ID especificado
+      var sheet = SpreadsheetApp.openById(spreadsheetId).getSheets()[0];
+  
+      // Adiciona uma nova linha com os dados especificados na planilha
+      sheet.appendRow(data);
+    } catch (error) {
+      // Em caso de erro, imprime a mensagem de erro no console
+      console.error(error);
+    }
+  }
 
-// put/update - atualizar dados
-function spreadsheet_update(){}
+// put/update - atualizar dados recebendo uma variavel que indica a linha que será alterada
+function spreadsheet_update(spreadsheetId, row, data) {
+    // Verifica se o ID da planilha é válido.
+    if (!spreadsheetId || !spreadsheetId.trim()) {
+      throw new Error("O ID da planilha é inválido.");
+    }
+  
+    // Obtém a planilha especificada pelo ID.
+    var sheet = SpreadsheetApp.openById(spreadsheetId).getSheets()[0];
+    if (!sheet) {
+      throw new Error("Não foi possível encontrar a planilha com o ID fornecido.");
+    }
+  
+    // Verifica se a linha especificada é válida.
+    if (row < 1 || row > sheet.getLastRow()) {
+      throw new Error("A linha especificada é inválida.");
+    }
+  
+    // Verifica se há dados suficientes para preencher a linha inteira.
+    var lastColumn = sheet.getLastColumn();
+    if (data.length < lastColumn) {
+      throw new Error("A matriz de dados fornecida é muito curta.");
+    }
+    
+    // Atualiza a linha especificada com os dados fornecidos.
+    sheet.getRange(row, 1, 1, lastColumn).setValues([data]);
+    }
 
-// delete - deletar dados
-function spreadsheet_delete(){}
+
+// delete - apagar dados
+function spreadsheet_delete(spreadsheetId, row) {
+  try {
+    // Verifica se o ID da planilha é válido
+    if (!spreadsheetId || typeof spreadsheetId !== 'string') {
+      throw new Error('O ID da planilha é inválido.');
+    }
+
+    // Verifica se a linha especificada é um número válido
+    if (!row || typeof row !== 'number') {
+      throw new Error('A linha especificada é inválida.');
+    }
+
+    var sheet = SpreadsheetApp.openById(spreadsheetId).getSheets()[0];
+    sheet.deleteRow(row);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
 
